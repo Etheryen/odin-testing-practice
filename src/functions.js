d53fcd0458ec4ee958fdafbcaeae04fe0ae83fdf
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-02-21 19:07:13 +0100

    done

---
+++ src/functions.js
@@ -0,0 +1,56 @@
+function capitalize(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+
+function reverseString(string) {
+  let result = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+
+  return result;
+}
+
+const calculator = (() => {
+  return {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+  };
+})();
+
+function isUpperCase(string) {
+  return string === string.toUpperCase();
+}
+
+function caesarCipher(string, shiftFactor) {
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const ALPHABET = alpha.map((x) => String.fromCharCode(x));
+
+  let result = '';
+
+  for (const char of string) {
+    if (ALPHABET.includes(char.toUpperCase())) {
+      let newChar =
+        ALPHABET[(ALPHABET.indexOf(char.toUpperCase()) + shiftFactor) % 26];
+      result += isUpperCase(char) ? newChar : newChar.toLowerCase();
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+function analyzeArray(array) {
+  return {
+    average: array.reduce((prev, cur) => prev + cur / array.length, 0),
+    min: Math.min(...array),
+    max: Math.max(...array),
+    length: array.length,
+  };
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
---
+++ src/functions.test.js
@@ -0,0 +1,65 @@
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from './functions';
+
+it('capitalizes', () => {
+  expect(capitalize('test string')).toBe('Test string');
+});
+
+it('reverses a string', () => {
+  expect(reverseString('test string')).toBe('gnirts tset');
+});
+
+it('adds', () => {
+  expect(calculator.add(10, 3)).toBe(13);
+});
+
+it('subtracts', () => {
+  expect(calculator.subtract(10, 3)).toBe(7);
+});
+
+it('miltiplies', () => {
+  expect(calculator.multiply(10, 3)).toBe(30);
+});
+
+it('divides', () => {
+  expect(calculator.divide(10, 3)).toBeCloseTo(3.33);
+});
+
+it('ciphers', () => {
+  expect(caesarCipher('defend the east wall of the castle', 2)).toBe(
+    'fghgpf vjg gcuv ycnn qh vjg ecuvng'
+  );
+});
+
+it('ciphers z to a', () => {
+  expect(caesarCipher('zzzz', 3)).toBe('cccc');
+  expect(caesarCipher('zzzz', 1)).toBe('aaaa');
+});
+
+it('ciphers respecting case', () => {
+  expect(caesarCipher('defEnd tHe East walL Of the cAstle', 2)).toBe(
+    'fghGpf vJg Gcuv ycnN Qh vjg eCuvng'
+  );
+});
+
+it('ciphers respecting punctuation', () => {
+  expect(
+    caesarCipher(`??defend; the, "east wall" 'of' the... castle!!!`, 2)
+  ).toBe(`??fghgpf; vjg, "gcuv ycnn" 'qh' vjg... ecuvng!!!`);
+});
+
+it('analyzes an array', () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+
+  expect(object).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
